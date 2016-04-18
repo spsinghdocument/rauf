1a39de62c2d7fb368c45f0e66faaf21e80bc5379
spsinghdocument <spsinghdocument@gmail.com>
2016-04-17 22:39:41 -0700

    first commit

---
+++ WebApplication22/WebApplication22/Controllers/POPUPController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using WebApplication22;
+
+namespace WebApplication22.Controllers
+{
+    public class POPUPController : Controller
+    {
+        //
+        // GET: /POPUP/
+  
+         
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 0)]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult chget()
+        {
+
+            mvc5Entities2 db = new mvc5Entities2();
+            var a = db.angcontrs.ToList();
+            //string Msg = ccls.challanoutvalidate(bill_no, c_date, country, "SIM", simno, AlotUser, sessid, branchcode, givento);
+            return Json(a, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
---
+++ WebApplication22/WebApplication22/Controllers/factoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication22.Controllers
+{
+    public class factoryController : Controller
+    {
+        //
+        // GET: /factory/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
